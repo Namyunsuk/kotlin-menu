da30aabb731af4a7610dddd7fadf81355237e47c
Namyunsuk <yunsuk0328@naver.com>
2023-12-14 16:30:45 +0900

    feat(CategoryInfo): 카테고리 정보 저장 및 특정번호에 대한 카테고리 정보 반환 기능 구현

---
+++ src/main/kotlin/menu/utils/CategoryInfo.kt
@@ -0,0 +1,15 @@
+package menu.utils
+
+enum class CategoryInfo(val category: String, val categoryNum: Int) {
+    JAPAN("일식", 1),
+    KOREA("한식", 2),
+    CHINA("중식", 3),
+    ASIA("아시안", 4),
+    WESTERN("양식", 5);
+
+    companion object {
+        fun match(categoryNum: Int): String {
+            return values().first { it.categoryNum == categoryNum }.category
+        }
+    }
+}
